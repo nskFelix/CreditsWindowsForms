2641a6e3878b2d0a2d4cb5f37c204e2c4672d613
Дмитрий <89134502005@mail.ru>
2021-02-03 18:16:29 +0700

    Добавлена функция редактировани для одной таблицы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,276 @@ namespace CreditsWindowsForms
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bankCB = new System.Windows.Forms.ComboBox();
+            this.devisionCB = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TestConncetionMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeAppMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabEdit = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cbTableToEdit = new System.Windows.Forms.ComboBox();
+            this.labelBank1 = new System.Windows.Forms.Label();
+            this.saveTableButton = new System.Windows.Forms.Button();
+            this.openTableButton = new System.Windows.Forms.Button();
+            this.dgEditDB = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabEdit.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEditDB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bankCB
+            // 
+            this.bankCB.FormattingEnabled = true;
+            this.bankCB.Location = new System.Drawing.Point(59, 3);
+            this.bankCB.Name = "bankCB";
+            this.bankCB.Size = new System.Drawing.Size(121, 21);
+            this.bankCB.TabIndex = 0;
+            this.bankCB.SelectedIndexChanged += new System.EventHandler(this.BankCB_SelectedIndexChanged);
+            // 
+            // devisionCB
+            // 
+            this.devisionCB.FormattingEnabled = true;
+            this.devisionCB.Location = new System.Drawing.Point(219, 3);
+            this.devisionCB.Name = "devisionCB";
+            this.devisionCB.Size = new System.Drawing.Size(121, 21);
+            this.devisionCB.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TestConncetionMenu,
+            this.closeAppMenu});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(166, 48);
+            // 
+            // TestConncetionMenu
+            // 
+            this.TestConncetionMenu.Name = "TestConncetionMenu";
+            this.TestConncetionMenu.Size = new System.Drawing.Size(165, 22);
+            this.TestConncetionMenu.Text = "Тест соединения";
+            // 
+            // closeAppMenu
+            // 
+            this.closeAppMenu.Name = "closeAppMenu";
+            this.closeAppMenu.Size = new System.Drawing.Size(165, 22);
+            this.closeAppMenu.Text = "Close";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1171, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.mainToolStripMenuItem.Text = "Main";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabEdit);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 24);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1171, 472);
+            this.tabControl.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1163, 446);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1163, 446);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabEdit
+            // 
+            this.tabEdit.Controls.Add(this.splitContainer1);
+            this.tabEdit.Location = new System.Drawing.Point(4, 22);
+            this.tabEdit.Name = "tabEdit";
+            this.tabEdit.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEdit.Size = new System.Drawing.Size(1163, 446);
+            this.tabEdit.TabIndex = 2;
+            this.tabEdit.Text = "Редактирование БД";
+            this.tabEdit.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cbTableToEdit);
+            this.splitContainer1.Panel1.Controls.Add(this.labelBank1);
+            this.splitContainer1.Panel1.Controls.Add(this.saveTableButton);
+            this.splitContainer1.Panel1.Controls.Add(this.openTableButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgEditDB);
+            this.splitContainer1.Size = new System.Drawing.Size(1157, 440);
+            this.splitContainer1.SplitterDistance = 186;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // cbTableToEdit
+            // 
+            this.cbTableToEdit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTableToEdit.FormattingEnabled = true;
+            this.cbTableToEdit.Items.AddRange(new object[] {
+            "Банки"});
+            this.cbTableToEdit.Location = new System.Drawing.Point(39, 3);
+            this.cbTableToEdit.Name = "cbTableToEdit";
+            this.cbTableToEdit.Size = new System.Drawing.Size(121, 21);
+            this.cbTableToEdit.TabIndex = 7;
+            // 
+            // labelBank1
+            // 
+            this.labelBank1.AutoSize = true;
+            this.labelBank1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBank1.Location = new System.Drawing.Point(48, 59);
+            this.labelBank1.Name = "labelBank1";
+            this.labelBank1.Size = new System.Drawing.Size(79, 24);
+            this.labelBank1.TabIndex = 6;
+            this.labelBank1.Text = "БАНКИ";
+            // 
+            // saveTableButton
+            // 
+            this.saveTableButton.Enabled = false;
+            this.saveTableButton.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveTableButton.Location = new System.Drawing.Point(94, 28);
+            this.saveTableButton.Name = "saveTableButton";
+            this.saveTableButton.Size = new System.Drawing.Size(90, 28);
+            this.saveTableButton.TabIndex = 2;
+            this.saveTableButton.Text = "Сохранить";
+            this.saveTableButton.UseVisualStyleBackColor = true;
+            this.saveTableButton.Click += new System.EventHandler(this.SaveTableButton_Click);
+            // 
+            // openTableButton
+            // 
+            this.openTableButton.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openTableButton.Location = new System.Drawing.Point(3, 28);
+            this.openTableButton.Name = "openTableButton";
+            this.openTableButton.Size = new System.Drawing.Size(92, 28);
+            this.openTableButton.TabIndex = 1;
+            this.openTableButton.Text = "Открыть";
+            this.openTableButton.UseVisualStyleBackColor = true;
+            this.openTableButton.Click += new System.EventHandler(this.OpenTableButton_Click);
+            // 
+            // dgEditDB
+            // 
+            this.dgEditDB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEditDB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgEditDB.Location = new System.Drawing.Point(0, 0);
+            this.dgEditDB.Name = "dgEditDB";
+            this.dgEditDB.Size = new System.Drawing.Size(967, 440);
+            this.dgEditDB.TabIndex = 0;
+            this.dgEditDB.CurrentCellChanged += new System.EventHandler(this.DgEditDB_CurrentCellChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1171, 496);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.devisionCB);
+            this.Controls.Add(this.bankCB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabEdit.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgEditDB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox bankCB;
+        private System.Windows.Forms.ComboBox devisionCB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TestConncetionMenu;
+        private System.Windows.Forms.ToolStripMenuItem closeAppMenu;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabEdit;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgEditDB;
+        private System.Windows.Forms.Button saveTableButton;
+        private System.Windows.Forms.Button openTableButton;
+        private System.Windows.Forms.Label labelBank1;
+        private System.Windows.Forms.ComboBox cbTableToEdit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,267 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace CreditsWindowsForms
 {
     public partial class Form1 : Form
     {
+        public static string conncetionString = @"Data Source=WORKNOTEBOOK\SQLEXPRESS;Initial Catalog=Credits;Integrated Security=True";
+        public SqlConnection sqlConnection = new SqlConnection(conncetionString);
+
         public Form1()
         {
             InitializeComponent();
+            cbTableToEdit.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BankFill();
+        }
+
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //открытие выбранной таблицы из БД 
+        private  void OpenTableButton_Click(object sender, EventArgs e)
+        {
+            string tableToEdit = cbTableToEdit.SelectedItem.ToString();
+            
+            switch (tableToEdit)
+            {
+                case "Банки":
+                    { LoadBanksTodgEditDB(); break; }
+                default: break;
+            }
+            saveTableButton.Enabled = false;
+            cbTableToEdit.Enabled = true;
+            openTableButton.Text = "Открыть";
+        }
+        
+        //Сохранение изменений DataGridView dgEditDB в БД
+        private void SaveTableButton_Click(object sender, EventArgs e)
+        {
+            string tableToEdit = cbTableToEdit.SelectedItem.ToString();
+            bool saved = false;
+            switch (tableToEdit)
+            {
+                case "Банки":
+                    { saved = SaveBankFromdgEditDB(); break; }
+                default: break;
+            }
+            if (saved)
+            {
+                saveTableButton.Enabled = false;
+                cbTableToEdit.Enabled = true;
+                openTableButton.Text = "Открыть";
+            }
+        }
+
+        //обнаружены изменения в DataGridView dgEditDB 
+        private void DgEditDB_CurrentCellChanged(object sender, EventArgs e)
+        {
+            saveTableButton.Enabled = true;
+            cbTableToEdit.Enabled = false;
+            openTableButton.Text = "Отмена";
+        }
+
+        //сохранение таблицы "Банки" в БД 
+        private bool SaveBankFromdgEditDB()
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader sqlReader = null;
+                SqlCommand command=null;
+
+                //Получение индеков, которые были в базе до сохранения
+                    List<string> baseID = new List<string>();
+                    command = new SqlCommand(@"SELECT BankID FROM Bank", sqlConnection);
+                    sqlReader = command.ExecuteReader();
+                    while (sqlReader.Read()) { baseID.Add(sqlReader["BankID"].ToString()); }
+                    sqlReader.Close();
+
+
+           //перенос данных из DataGridView в БД
+                foreach (DataGridViewRow i in dgEditDB.Rows)
+                {
+                    if (!i.IsNewRow)
+                    {
+                        
+                        string id = i.Cells[0].Value.ToString();
+                        string name = i.Cells[1].Value.ToString();
+
+                        if (baseID.Contains(id))
+                        {
+                            command = new SqlCommand($@"UPDATE Bank SET Name='{name}' WHERE BankID={id}", sqlConnection);
+                            baseID.Remove(id);
+                        }
+                        else
+                        {
+                            command = new SqlCommand($@"INSERT INTO Bank VALUES ('{name}')", sqlConnection);
+                        }
+                        sqlReader = command.ExecuteReader();
+                        sqlReader.Close();
+                    }
+                }
+
+           //Удаление строк найденых в БД, но отсутствующих в DataGridView
+                foreach (string id in baseID)
+                {
+                        command = new SqlCommand($@"DELETE FROM Bank  
+                                                            WHERE BankID={id}",
+                                                            sqlConnection);
+                        
+                        sqlReader = command.ExecuteReader();
+                        sqlReader.Close();
+                }
+
+               
+                sqlConnection.Close();
+                LoadBanksTodgEditDB();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        //загрузка таблицы "Банки" из БД и настройка столбцов DataGridView dgEditDB под ограничения этой таблицы 
+        private  void LoadBanksTodgEditDB()
+        {
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand(@"SELECT * 
+                                                  FROM Bank
+                                                  ORDER BY BankID",
+                                                  sqlConnection);
+            try
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds);
+                dgEditDB.DataSource = ds.Tables[0];
+                dgEditDB.Columns[0].ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            sqlConnection.Close();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            BankFill();
+        }
+
+        private async void BankFill()
+        {
+            bankCB.Items.Clear();
+            await sqlConnection.OpenAsync();
+
+            SqlDataReader sqlReader = null;
+            SqlCommand command = new SqlCommand(@"SELECT Name 
+                                                  FROM Bank
+                                                  ORDER BY BankID",
+                                                  sqlConnection);
+            try
+            {
+                sqlReader = await command.ExecuteReaderAsync();
+                while (await sqlReader.ReadAsync())
+                {
+                    bankCB.Items.Add(sqlReader["Name"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+
+            }
+            sqlConnection.Close();
+            try
+            {
+                bankCB.SelectedIndex = 0;
+            }
+            catch { }
+        }
+
+        private async void DevisionFill(string bankName)
+        {
+            devisionCB.Items.Clear();
+            await sqlConnection.OpenAsync();
+
+            SqlDataReader sqlReader = null;
+            SqlCommand command = new SqlCommand($@"SELECT bd.Name
+                                                   FROM BankDevision as bd
+                                                   JOIN Bank as b ON b.BankID=bd.BankID
+                                                   WHERE b.Name='{bankName}'",
+                                                   sqlConnection);
+
+            try
+            {
+                sqlReader = await command.ExecuteReaderAsync();
+                while (await sqlReader.ReadAsync())
+                {
+                    devisionCB.Items.Add(sqlReader["Name"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+
+            }
+            sqlConnection.Close();
+            try
+            {
+                devisionCB.SelectedIndex = 0;
+            }
+            catch { }
+        }
+
+        private void BankCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DevisionFill(bankCB.Text.ToString());
         }
     }
 }
